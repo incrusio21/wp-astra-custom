b83c950019d881b75f2d4dbd27d02372949113b4
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-08-17 20:08:26 +0800

    init: custom halaman login dan register

---
+++ ultimate-member/templates/register-custom.php
@@ -0,0 +1,32 @@
+<?php 
+/* Template: Register Custom */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! is_user_logged_in() ) {
+	um_reset_user();
+}
+?>
+
+<div class="um <?php echo esc_attr( $this->get_class( $mode ) ); ?> um-<?php echo esc_attr( $form_id ); ?>">
+	<div class="um-form" data-mode="<?php echo esc_attr( $mode ); ?>">
+		<form method="post" action="">
+			<?php
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( 'um_before_form', $args );
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( "um_before_{$mode}_fields", $args );
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( "um_main_{$mode}_fields", $args );
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( 'um_after_form_fields', $args );
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( "um_after_{$mode}_fields", $args );
+			/** This action is documented in includes/core/um-actions-profile.php */
+			do_action( 'um_after_form', $args );
+			?>
+		</form>
+	</div>
+</div>
